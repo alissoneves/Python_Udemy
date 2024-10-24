6c03e9d59b7b8d1bde4a1384fa7010ac43914a4b
alissoneves <alissoneves_it@outlook.com>
2024-10-24 19:32:18 -0300

    Curso_Python_Commit_4

---
+++ Aula_Aritemetica.py
@@ -0,0 +1,24 @@
+adicao = 10 + 10
+print ('Adição', adicao)
+
+subtracao = 104 - 10
+print ('Subtração', subtracao)
+
+multiplicacao = 104 * 10
+print ('Multiplicação', multiplicacao)
+
+divisao = 10 / 2 #float
+print ('Divisão', divisao)
+
+divisao = 10 / 2 #float
+print ('Divisão', divisao)
+
+divisao_inteira = 10 // 2.2
+print ('Divisão Inteira', divisao_inteira)
+
+exponencial = 2 ** 10
+print ('exponencial', exponencial)
+
+modulo = 25 % 5 #resto da divisão
+print ('Módulo', modulo)
+
