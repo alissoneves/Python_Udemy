959e71c91b1c0c7f4912ddae2c8f1a3e8584361d
alissoneves <alissoneves_it@outlook.com>
2024-10-24 18:57:45 -0300

    Curso_Python_Commit_2

---
+++ Exercicio1.py
@@ -0,0 +1,18 @@
+
+nome = 'João Mauro'
+sobrenome = 'Silveira'
+idade = 88
+ano_nascimento = 1914
+maior_idade = idade >= 20
+altura_metros = '1.79'
+
+
+print ('Nome:', nome)
+print ('Sobrenome:', sobrenome)
+print ('Idade:', idade)
+print ('Ano de Nascimento:', ano_nascimento)
+print ('É maior de idade?:', maior_idade)
+print ('Altura em Metros:', altura_metros)
+
+
+
