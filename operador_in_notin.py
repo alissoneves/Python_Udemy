9b11b87bd31c1b192ba295f23b79a24fb3235cc1
alissoneves <alissoneves_it@outlook.com>
2024-10-25 18:49:57 -0300

    Curso_Python_Commit_13

---
+++ operador_in_notin.py
@@ -0,0 +1,5 @@
+nome = 'Otávio'
+
+print ('v' in nome) 
+print ('vio' not in nome)
+
---
+++ operador_not.py
@@ -0,0 +1,10 @@
+# Operador lógico "not"
+# Usado para inverter expressões
+# not True = False
+# not False = True
+# senha = input('Senha: ')
+
+senha = input('Digite sua senha: ')
+    
+if not senha:
+        print('Você não digitou sua senha, favor avaliar! ')
