636df767f58c5a73a3f3b31ca143fee42ac99e4d
alissoneves <alissoneves_it@outlook.com>
2024-10-25 15:48:57 -0300

    Curso_Python_Commit_6

---
+++ f_string.py
@@ -0,0 +1,9 @@
+nome = 'Alisson'
+altura = 1.79
+peso = 51
+imc = peso / altura ** 2
+
+linha_1 = f'{nome} tem {altura: .2f} de altura'
+
+print (linha_1)
+print ( 'pesa', peso, 'quilos e seu IMC é de:', imc)
