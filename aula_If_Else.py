0a9f1e7380aeb31427bc4c1abfa780a5ededc54a
alissoneves <alissoneves_it@outlook.com>
2024-10-25 17:01:23 -0300

    Curso_Python_Commit_9

---
+++ aula_If_Else.py
@@ -0,0 +1,12 @@
+entrada = input ('Você quer "Entrar" ou "Sair"? ')
+
+
+if entrada == 'entrar': 
+    print ('Você escolher entrar, parabéns guerreiro!')
+
+elif entrada == 'sair':
+    print ('Você quer sair, te vejo na próxima aventura guerreiro!')
+
+else: 
+    print ('Você não digitou nenhuma das respostas válidas, favor retornar!')
+
