25739aeeb19970c19c9a567b11ea814380653ea8
alissoneves <alissoneves_it@outlook.com>
2024-10-25 18:29:35 -0300

    Curso_Python_Commit_12

---
+++ operador_and.py
@@ -0,0 +1,32 @@
+# Operadores lógicos
+# and (e) or (ou) not (não)
+# and - Todas as condições precisam ser
+# verdadeiras.
+# Se qualquer valor for considerado falso,
+# a expressão inteira será avaliada naquele valor
+# São considerados falsy (que vc já viu)
+# 0 0.0 '' False
+# Também existe o tipo None que é
+# usado para representar um não valor
+# entrada = input('[E]ntrar [S]air: ')
+# senha_digitada = input('Senha: ')
+
+
+entrada = input('[E]ntrar [S]air: ')
+
+
+if entrada == 'E':
+    senha_digitada = input('Senha:')
+    senha_permitida = '123456'
+    
+if entrada == 'E' and senha_digitada  == senha_permitida:
+
+    print('Bem vindo ao ambiente') 
+    
+
+if entrada == 'E' and senha_digitada != senha_permitida:
+
+    print('Você não está permitido a entrar no sistema, verifique sua senha.')
+
+if entrada == 'S':
+        print('Volte sempre jogador!') 
