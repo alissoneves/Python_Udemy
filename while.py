96d4e492fbb087bcf76aa9373dc59f006454bfc3
alissoneves <alissoneves_it@outlook.com>
2024-10-30 19:44:10 -0300

    Curso_Python_Commit_22

---
+++ while.py
@@ -0,0 +1,13 @@
+"""
+While (Enquanto)
+Executa uma ação enquanto uma condição for verdadeira.
+"""
+
+contador = 0
+
+while contador <= 9: 
+
+    contador = contador + 1
+    print (contador)
+
+print ('Acabou a contagem!')
