3e1c8f0ce2a7303cdef00cbe05e30986c4a3b24b
alissoneves <alissoneves_it@outlook.com>
2024-11-06 20:59:30 -0300

    Curso_Python_Commit_29

---
+++ listas_aula80.py
@@ -0,0 +1,6 @@
+primeira_lista = [] #lista vazia
+segunda_lista = [123, True, 'Luiz Otávio', 1.2, []]
+
+print(primeira_lista)
+print (segunda_lista)
+print (segunda_lista [0])
---
+++ listas_aula81.py
@@ -0,0 +1,6 @@
+lista = [1, 2, 3, 4]
+lista[2] = 300
+del lista [1]
+print (lista [2])
+print (lista)
+
