9bb0910fdad92729783d55616b722e96f0da4066
alissoneves <alissoneves_it@outlook.com>
2024-11-06 21:32:43 -0300

    Curso_Python_Commit_30

--- listas_aula81.py
+++ listas_aula81.py
@@ -1,6 +1,13 @@
+#Utilizando Del para deletar algo na sua lista.
+#Utilizando POP você deleta o ultimo valor da sua lista.
+#Utilizando o APPEND para incluir algo na lista.
+
 lista = [1, 2, 3, 4]
 lista[2] = 300
 del lista [1]
 print (lista [2])
 print (lista)
-
+lista.append (400)
+print (lista)
+lista.pop ()
+print (lista)
---
+++ listas_aula82.py
@@ -0,0 +1,6 @@
+#utilizando INSERT você vai inserir algo em um determinado indice. 
+
+
+lista = [10, 20, 30, 40]
+lista.insert (0, 'Casa')
+print (lista)
