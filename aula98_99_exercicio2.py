9f0231bd930377d19f4b4c175316f8088987198a
alissoneves <alissoneves_it@outlook.com>
2024-11-12 19:24:24 -0300

    Curso_Python_Commit_36

---
+++ aula98_99_exercicio2.py
@@ -0,0 +1,40 @@
+"""
+Calculo do primeiro dígito do CPF
+CPF: 746.824.890-70
+Colete a soma dos 9 primeiros dígitos do CPF
+multiplicando cada um dos valores por uma
+contagem regressiva começando de 10
+
+Ex.:  746.824.890-70 (746824890)
+   10  9  8  7  6  5  4  3  2
+*  7   4  6  8  2  4  8  9  0
+   70  36 48 56 12 20 32 27 0
+
+Somar todos os resultados: 
+70+36+48+56+12+20+32+27+0 = 301
+Multiplicar o resultado anterior por 10
+301 * 10 = 3010
+Obter o resto da divisão da conta anterior por 11
+3010 % 11 = 7
+Se o resultado anterior for maior que 9:
+    resultado é 0
+contrário disso:
+    resultado é o valor da conta
+
+O primeiro dígito do CPF é 7
+"""
+
+
+
+cpf = '74682489070'
+nove_digitos = cpf[1:8]
+contador_regressivo_2 = 10
+
+resultado_digito_2 = 0 
+for digito_2 in nove_digitos:
+    resultado_digito_2 += int(digito_2) * contador_regressivo_2
+    contador_regressivo_2 -= 1
+digito_2 = (resultado_digito_2 * 10) % 11    
+digito_2 = digito_2 if digito_2 <= 9 else 0
+
+print (f'O segundo dígito do seu CPF é:', digito_2)
