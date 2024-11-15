aaddbfb01c1039a9d9166b1a7c4207aa325755ac
alissoneves <alissoneves_it@outlook.com>
2024-11-14 21:13:16 -0300

    Curso_Python_Commit_40

---
+++ aula105_funcao.py
@@ -0,0 +1,15 @@
+'''
+Introdução às funções (def) em Python
+Funções são trechos de código usados para replicar determinada
+ação ao longo do seu código.
+Elas podem receber valores para parametros (Argumentos)
+e retonar um valor especifico.
+Por padrão, funções Python retornam None (nada).
+'''
+
+def imprimir():
+
+    print ('Várias Vezes Executando')
+    print ('Testando dentro da def')
+
+imprimir ()
