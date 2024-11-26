0f5e7e1342d8b1e25790375af7212b5c2b93c47a
alissoneves <alissoneves_it@outlook.com>
2024-11-25 21:33:20 -0300

    Curso_Python_Commit_53

--- Curso_Guanabara/exercicio_37.py
+++ Curso_Guanabara/exercicio_37.py
@@ -7,33 +7,16 @@ conversor_escolhido = int(input(
     'Sua Opção: '))
 
 if conversor_escolhido == 1: 
-    binario = ""
-    while numero_escolhido > 0:
-        resto = numero_escolhido % 2
-        binario = str(resto) + binario
-        numero_escolhido //= 2
 
-    print (f'{numero_escolhido} convertido para BINÁRIO é igual a {binario}')
+    print (f'{numero_escolhido} convertido para BINÁRIO é igual a {bin(numero_escolhido)[2:]}')
 
 elif conversor_escolhido == 2:
 
-    octal = ""
-    while numero_escolhido > 0:
-        resto = numero_escolhido % 8
-        octal = str(resto) + octal
-        numero_escolhido //= 8
-    print (f'{numero_escolhido} convertido para OCTAL é igual a {octal}')
+    print (f'{numero_escolhido} convertido para OCTAL é igual a {oct(numero_escolhido)[2:]}')
+
+elif conversor_escolhido == 3:
+
+    print (f'{numero_escolhido} convertido para OCTAL é igual a {hex(numero_escolhido)[2:]}') 
 
 else: 
-    conversor_escolhido == 3
-
-    hexa = ""
-    while numero_escolhido > 0:
-        resto = numero_escolhido % 16
-    if resto < 10:
-        hexa = str(resto) + hexa
-    else:
-        hexa = chr(55 + resto) + hexa  # Converte 10-15 em A-F
-        numero_escolhido//= 16
-
-    print (f'{numero_escolhido} convertido para OCTAL é igual a {hexa}') 
+    print ('Você não digitou nenhuma opção válida, favor tentar novamente!')    
